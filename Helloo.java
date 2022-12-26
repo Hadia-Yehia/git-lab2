b8c0657b33abd8023fcfc77e0c0cf1705dd32df5
Farida <farida.bahi@gmail.com>
2022-12-26 14:33:13 +0200

    Added Java file on branch HelloLab2

---
+++ Helloo.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+	public void helloGit()
+	{
+		System.out.println("Hello Git");
+	}
+	public static void main(String[] args)
+	{
+		helloGit();
+		System.out.println("Hello World");
+	}
+}
